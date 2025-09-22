1ba11dcc899b6d0dccbfcceb3f9c167fd7ad1322
david Gross <dgross1386@gmail.com>
2025-09-22 10:48:03 -0400

    Welcome to DevAsc

---
+++ PyScripts/basic_script.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Basic Python Script
+Author: dgross-hub
+Date: September 22, 2025
+"""
+
+def main():
+    print("Hello, world!")
+    print("Welcome to DEVASC!")
+
+if __name__ == "__main__":
+    main()
